d3c32788def4ed70cee40ba65411d10baf54c421
Manoj <punmanoj2060@gmail.com>
2025-07-12 09:22:58 +0545

    .

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
       <div className="flex flex-col items-center px-6 py-0">
         <h1 className="text-3xl font-bold text-orange-500 mb-4 text-center">
-          Task 2 !! Task 2 !! Task 2 !! 
+          Task 2 !! Task 2 !! Task 2 !! please wait for moment since image might take time loading
         </h1>
         <Task2 />
       </div>
